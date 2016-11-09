cef9a553fc53ca47743e723dd60b0dc146b759f0
TehPwns <omgtehpwns@yahoo.com>
2016-11-08 22:45:09 -0500

    Refactor drawer registry; update .gitignore

--- src/draw/draw.cpp
+++ src/draw/draw.cpp
@@ -9,48 +9,33 @@ namespace draw
 
 /* Drawer registration
  * Drawers are enabled by adding them to the drawerRegistry object.
- * The map related a drawer type to a name, and a function that, when called,
- * returns and instance of the drawer. */
+ * It relates a type enum to DrawerRegistry object, which contains
+ * a name and a creation function (returns unique_ptr) */
 
 //A function that creates a drawer
 typedef std::function<std::unique_ptr<BaseDrawer>(void)> DrawerCreatorFn;
 
-//Small type to store name and creation information about a drawer
 struct DrawerRegistry
 {
-public:
-    //Main function used to create a DrawerRegistry instance.
-    //The reason is because DrawerRegistry cannot be a template object
-    //itself in order to fit into the drawerRegistry map
-    template<typename DrawerType>
-    static DrawerRegistry create(const std::string& name) 
-    {
-        return DrawerRegistry { name, GetDrawerCreatorFn<DrawerType>() };
-    }
-
-public:
-    //User-friendly Name of drawer, specified on command line
     std::string name;
-
-    //Function to create an instance of this drawer
-    DrawerCreatorFn creator;
-
-private:
-    //Helper to return a function that returns an instance of a drawer
-    // Used to assign to DrawerRegistry::creator
-    template<typename DrawType>
-    static constexpr DrawerCreatorFn GetDrawerCreatorFn()
-    {
-        return [](){ return std::make_unique<DrawType>(); };
-    }
+    DrawerCreatorFn create;
 };
+
+template<typename DrawerType>
+DrawerRegistry makeDrawerRegistry(const std::string& name)
+{
+    return DrawerRegistry {
+        name,
+        []() { return std::make_unique<DrawerType>(); }
+    };
+}
 
 //Main object to register a drawer. 
 static const std::map<DrawerType, DrawerRegistry> drawerRegistry = 
 {
-    { DrawerType::Normal,    DrawerRegistry::create<NormalDrawer>("normal")    },
-    { DrawerType::HeightMap, DrawerRegistry::create<HeightmapDrawer>("height") },
-    { DrawerType::Shaded,    DrawerRegistry::create<ShadedDrawer>("shaded")    }       
+    { DrawerType::Normal,    makeDrawerRegistry<NormalDrawer>("normal")    },
+    { DrawerType::HeightMap, makeDrawerRegistry<HeightmapDrawer>("height") },
+    { DrawerType::Shaded,    makeDrawerRegistry<ShadedDrawer>("shaded")    }       
 };
 
 /* ------------------------------------------------------------------------- */
@@ -69,11 +54,11 @@ DrawerCreatorFn::result_type createDrawer(DrawerType type)
 {
     auto it = drawerRegistry.find(type);
     if(it != drawerRegistry.end()) {
-        return it->second.creator();
+        return it->second.create();
     }
 
     //Exception - should not happen
-    error("Invalid drawer type \"", int(type));    
+    error("Invalid drawer type \"", int(type), "\"");    
 }
 
 DrawerType getDrawerType(const std::string& drawerName)
